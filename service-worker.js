6c85272a16c2b76c3c71ca693a17ff7269ef559a
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-06-14 03:28:43 +0200

    Add static app service worker

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'credly-growth-hub-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
