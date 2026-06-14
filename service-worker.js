678b9888c9c7da942f9e5c33e6df72ace806b4d2
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-06-14 03:50:56 +0200

    Bump app cache after UX refresh

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'credly-growth-hub-v1';
+const CACHE_NAME = 'credly-growth-hub-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
@@ -9,6 +10,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
